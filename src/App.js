ce5dfb2d360aa722353129c088f3550e6e7c5af4
danilopetrovic88 <vivify@VivifyIdeass-MacBook-Pro-3.local>
2022-06-27 21:11:34 +0200

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppCars from './components/AppCars';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Router>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to={'/cars'}>Cars</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route path={'/cars'}>
+            <AppCars  />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
